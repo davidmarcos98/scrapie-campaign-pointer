126d659d752b979fd54ff9eebbb82ac0e39bbd7d
David Marcos <david.mazon@un.org>
2024-07-17 11:59:51 +0200

    test

--- process_campaign.js
+++ process_campaign.js
@@ -11,6 +11,8 @@ const POINTS_TOP_10 = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10];
 
 const data = {};
 
+console.log(process.env.TEST);
+
 const getCampaign = async () => {
   let client = new TMIO.Client();
   let campaign = await client.campaigns.get(
